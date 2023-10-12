7879cc06be9ad1fb7d512ee7a44f03d147512421
makima <konyahoshi@qq.com>
2023-10-12 22:48:37 +0800

    update page key

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export const CAPSOLVER_KEY = ""
 // 因为接入了三方接码平台，所以这里的key需要自己去申请，费用大概是1美元100次，如果低于100个号，充个5美元就够了，不介意可以走我的邀请链接，谢谢。
 // https://dashboard.capsolver.com/passport/register?inviteCode=5RABgFowf082 
 
-export const PAGE_KEY = "6LeeU2MoAAAAAPKdFRxDi5Ga3BkxNHc0CMpklEoQ"
+export const PAGE_KEY = "6LeETIcoAAAAAImuDxmImELck_GPUKzVA9Kz186G"
 // PAGE_KEY可能会发生变化，可以在 https://genesis.celestia.org/ 页面打开浏览器开发者工具（F12），点击"Check TIA eligibility" -> 点击"Check address manually"
-// 在network中找到 https://www.google.com/recaptcha/api2/reload?k=6LeeU2MoAAAAAPKdFRxDi5Ga3BkxNHc0CMpklEoQ 请求，k=后面的值即可为最新的的PAGE_KEY
+// 在network中找到 https://www.google.com/recaptcha/api2/reload?k=6LeETIcoAAAAAImuDxmImELck_GPUKzVA9Kz186G 请求，k=后面的值即可为最新的的PAGE_KEY
